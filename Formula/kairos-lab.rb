dc7ab6c45990a0785ffcf5912bc41bbce4f89ad6
mudler <2420543+mudler@users.noreply.github.com>
2026-04-03 12:18:56 +0000

    kairos-lab 0.1.0-rc.9

--- Formula/kairos-lab.rb
+++ Formula/kairos-lab.rb
@@ -5,20 +5,20 @@ class KairosLab < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/kairos-io/kairos-lab/releases/download/v0.1.0-rc.8/kairos-lab_0.1.0-rc.8_darwin_arm64.tar.gz"
-      sha256 "8929eb2b23d5defb0dfe4f9f6ed4906eab111e5786b2e719a0665758f89d3207"
+      url "https://github.com/kairos-io/kairos-lab/releases/download/v0.1.0-rc.9/kairos-lab_0.1.0-rc.9_darwin_arm64.tar.gz"
+      sha256 "6de9a181d2034292690d2035b55f01e1e2f06ca8c185cc75c9b40c95144ae2f1"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/kairos-io/kairos-lab/releases/download/v0.1.0-rc.8/kairos-lab_0.1.0-rc.8_linux_amd64.tar.gz"
-      sha256 "f41e810e682ae8c411e321aee58ffe2296f7cffeceb71b6d23a4e7616fc4fb31"
+      url "https://github.com/kairos-io/kairos-lab/releases/download/v0.1.0-rc.9/kairos-lab_0.1.0-rc.9_linux_amd64.tar.gz"
+      sha256 "dfb84c2d75d06380987980d29cf7326b14c1c6ee2aeb5fd55e5c117b842b23e3"
     end
 
     on_arm do
-      url "https://github.com/kairos-io/kairos-lab/releases/download/v0.1.0-rc.8/kairos-lab_0.1.0-rc.8_linux_arm64.tar.gz"
-      sha256 "4c89fc5b74198e32324ea6633d1cd60c20110e15bfd746b478e5c98607387b75"
+      url "https://github.com/kairos-io/kairos-lab/releases/download/v0.1.0-rc.9/kairos-lab_0.1.0-rc.9_linux_arm64.tar.gz"
+      sha256 "01c6d3df69ca738554c46252b93814467786ade3ea1a6613850a31f8308aade9"
     end
   end
 
